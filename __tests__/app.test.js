3b0b5274e4407c741bec38e835d62b54c2603f31
olivia pasion <oap9820@gmail.com>
2022-08-02 18:04:35 -0700

    seeding data

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,6 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
+const { request } = require('express');
 // const request = require('supertest');
 // const app = require('../lib/app');
 
@@ -7,10 +8,14 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
-  });
-  afterAll(() => {
-    pool.end();
+
+  it('GET /books should return a list of books', async () => {
+    const resp = await request(app).get('/books');
+    expect(resp.body.length).toEqual(4);
+    expect(resp.body[0]).toEqual({
+      id: expect.any(String),
+      title: expect.any(String),
+      released: expect.any(Number)
+    });
   });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,19 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP table if exists books;
+
+CREATE table books (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR NOT NULL,
+  released INT NOT NULL
+);
+
+INSERT INTO books (title, released) VALUES
+('Good Omens', 1990),
+('The Guernsey', 2007),
+('The Talisman', 1984),
+('Beyond the End of the World', 2022)
+
+
+
